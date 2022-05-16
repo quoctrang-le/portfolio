1a30eebb6d6132c48a9a1ca2102e45eec2cf49eb
lequoctrang <quoctrang2701@gmail.com>
2022-05-16 16:24:12 +0700

    add works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 const parallax = document.querySelector('.parallax__wrapper')
 
-window.addEventListener('scroll', function () {
-  let offset = window.pageYOffset
+window.addEventListener('scroll', function() {
+    let offset = window.pageYOffset
 
-  parallax.style.backgroundPositionY = offset * 0.7 + 'px'
+    parallax.style.backgroundPositionY = offset * 0.7 + 'px'
 })
 
 const texts = ['HTML', 'CSS', 'JAVASCRIPT', 'REACTJS']
@@ -12,46 +12,76 @@ let index = 0
 let currentText = ''
 let letter = ''
 
-;(function type() {
-  if (count === texts.length) {
-    count = 0
-  }
-  currentText = texts[count]
-  letter = currentText.slice(0, ++index)
-  document.querySelector('.dynamic').textContent = letter
-  if (letter.length === currentText.length) {
-    count++
-    index = 0
-  }
-  setTimeout(type, 100)
+;
+(function type() {
+    if (count === texts.length) {
+        count = 0
+    }
+    currentText = texts[count]
+    letter = currentText.slice(0, ++index)
+    document.querySelector('.dynamic').textContent = letter
+    if (letter.length === currentText.length) {
+        count++
+        index = 0
+    }
+    setTimeout(type, 100)
 })()
 
 const counters = document.querySelectorAll('.countUp')
 
 counters.forEach((counter) => {
-  counter.innerText = '0'
+    counter.innerText = '0'
 
-  const updateCounter = () => {
-    const target = counter.getAttribute('data-target') // value count to
-    const value = +counter.innerText // current value
-    const increment = target / 300 // how fast is nubmer increment
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target') // value count to
+        const value = +counter.innerText // current value
+        const increment = target / 500 // how fast is nubmer increment
 
-    if (value < target) {
-      counter.innerText = `${Math.ceil(value + increment)}`
-      setTimeout(updateCounter, 2)
-    } else {
-      counter.innerText = target
+        if (value < target) {
+            counter.innerText = `${Math.ceil(value + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
     }
-  }
-  window.addEventListener('scroll', () => {
-    if (pageYOffset >= 1750 && pageYOffset <= 2000) updateCounter()
-  })
+    window.addEventListener('scroll', () => {
+        if (pageYOffset >= 2300) updateCounter()
+    })
 })
 
-const pre = document.getElementById('preload')
-function preloader() {
-  setTimeout(() => {
-    pre.style.display = 'none'
-  }, 2000)
+// const pre = document.getElementById('preload')
+
+// function preloader() {
+//     setTimeout(() => {
+//         pre.style.display = 'none'
+//     }, 3000)
+// }
+// preloader()
+
+
+
+
+const lists = document.querySelectorAll('.list')
+const items = document.querySelectorAll('.itemBox')
+
+for (let i = 0; i < lists.length; i++) {
+    lists[i].addEventListener('click', function() {
+        for (let j = 0; j < lists.length; j++) {
+            lists[j].classList.remove('active')
+        }
+        this.classList.add('active')
+
+        let dataFilter = this.getAttribute('data-filter')
+        console.log(dataFilter)
+        for (let k = 0; k < items.length; k++) {
+            items[k].classList.remove('show')
+            items[k].classList.add('hide')
+
+            if (items[k].getAttribute('data-item') == dataFilter || dataFilter == 'all') {
+                console.log(items[k].getAttribute('data-item'))
+                items[k].classList.remove('hide')
+                items[k].classList.add('show')
+            }
+        }
+    })
 }
-preloader()
